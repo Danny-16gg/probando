47fec6097523f1dd0434dc889725f7c81dc5131a
Danny-16gg" <ccorte22@estudiante.iberoamericana.edu.co>
2025-03-14 16:48:53 -0500

    primet commit

---
+++ github/src/github/ejercicio00.java
@@ -0,0 +1,34 @@
+/*ejercicio00*/
+package github;
+
+
+public class ejercicio00 {
+
+   
+    public static void main(String[] args) {
+        //java
+        /*cualquiera de estas dos sintaxis se puede utilizar para comentarios*/
+        
+        String hola;//variable en java
+        final String adios;//constante en java
+        
+        float decimal= 2.45f;// se escribe una f al final del valor de asignacion 
+        double decimal2= 2.14;// es un tipo de dato en decimal mas exacto 
+        String textoOCadena = "hi,word";
+        boolean si= true;
+        boolean no= false;
+        int num1 = 2;
+        char letra = 2; //char es un tipo de dato unico puede ser un solo numero o letra
+        byte num2 = 123; // tipo de dato de numero que va del -128 a 127
+        short num3 = 3; //tipo de dato de numero -2^15
+        long num4 = 5; //tipo de dato de numero -2^63 a 2^63
+        
+        System.out.println(textoOCadena);
+        
+        
+       
+        
+        
+ 
+    }
+}
